ee9fac0b2ecf7bed30a6aab05bf0c8e4c76b9a53
Rashithsilva <75090717+Rashithsilva@users.noreply.github.com>
2020-11-26 23:19:44 +0530

    Add files via upload

---
+++ Scripts/J.js
@@ -0,0 +1,12 @@
+function clickfunction(){
+    var a= document.getElementById("Hnav");
+    if(a.className === "HigNav"){
+        a.className += " responsive";   
+    }
+    else{
+        a.className = "HigNav";
+
+
+        
+    }   
+}
